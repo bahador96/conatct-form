a5018fa6ec63686d912ac50217ebfd5c295c4434
Bahador Fouladi <bahador.foladi2@gmail.com>
2024-08-31 20:22:25 +0330

    rm comments

--- src/Components/ConfirmModal.jsx
+++ src/Components/ConfirmModal.jsx
@@ -1,26 +1,3 @@
-// import React from "react";
-// import styles from "./ConfirmModal.module.css";
-
-// const ConfirmModal = ({ onConfirm, onCancel }) => {
-//   return (
-//     <div className={styles.overlay}>
-//       <div className={styles.modal}>
-//         <p>شما در حال افزودن مخاطب هستید. آیا مطمئن هستید؟</p>
-//         <div className={styles.buttons}>
-//           <button onClick={onConfirm} className={styles.confirmButton}>
-//             افزودن
-//           </button>
-//           <button onClick={onCancel} className={styles.cancelButton}>
-//             انصراف
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ConfirmModal;
-
 import React from "react";
 import styles from "./ConfirmModal.module.css";
 
